868685c2ed0538675630337a70ee164df25bd229
David López Felguera <davidlopez.david@gmail.com>
2016-01-31 13:12:55 +0100

    Update raml

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var shell = require('gulp-shell')
+var shell = require('gulp-shell');
 
 gulp.task('runOauth2Server', shell.task('nodemon --watch ./ server.js'));
 gulp.task('raml2html', shell.task('./node_modules/raml2html/bin/raml2html api.raml > api.html'));
